fe8c8d0598f8a7042dba3ab4b319ec32f8b9ccae
Farrah Lee <farrahyglee@yahoo.com>
2015-09-16 05:10:10 +0000

    Update

---
+++ app/controllers/webpage_controller.rb
@@ -0,0 +1,7 @@
+class WebpageController < ApplicationController
+	def index
+	end
+
+	def new
+	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Webpage::Application.routes.draw do
+
+  root 'webpage#index'
+
+  resources :webpage
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
